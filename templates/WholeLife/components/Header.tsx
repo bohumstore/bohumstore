db2bf69f82a57d928741a73c3453eef85dd773e8
bohumstore <urisky1@naver.com>
2025-07-19 12:13:56 +0900

    템플릿 생성

---
+++ templates/WholeLife/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="w-full flex items-center justify-center py-6 px-4 md:px-12 bg-[#f8f8f8] border-b border-gray-200">
+      <Image src="/bohumstore-logo.png" alt="보험스토어 로고" width={220} height={60} priority />
+    </header>
+  )
+}
