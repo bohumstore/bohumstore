ed166c0405072b53d5cf2db2301151d3ef9a8359
bohumstore <urisky1@naver.com>
2025-08-19 14:08:28 +0900

    feat: 네이버 파워링크 광고용 파비콘 설정 추가 - 다양한 크기의 파비콘 파일들 추가 (16x16, 32x32, 48x48, apple-touch-icon) - layout.tsx에 파비콘 링크 태그들 추가 - 네이버 파워링크에서 사이트 옆 파비콘 표시 지원

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,15 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="보험스토어" />
+        
+        {/* 파비콘 설정 - 네이버 파워링크 광고용 */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
